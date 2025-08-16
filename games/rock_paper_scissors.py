355e5db02c268d7f1691e3e6e0f9b00f7c9ec400
Mondrita Ghosh <gmondrita.mg@gmail.com>
2025-08-16 17:17:44 +0530

    Update rock_paper_scissors.py

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -1,1 +1,17 @@
-print("Rock, Paper, Scissors Game") 
+import random
+print("Rock Paper Scissors Game! >U<")
+choices = ["rock", "paper", "scissor"]
+name = input("enter your name: ").title()
+user = input("enter rock, paper or scissors: ").lower()
+comp = random.choice(choices)
+
+if user == comp:
+    print("it's a tie!")
+elif (user == "rock" and comp == "scissor"):
+    print(f"User {name} wins the move {user}!")
+elif (user == "paper" and comp == "rock"): 
+    print(f"User {name}  wins the move {user}!")   
+elif (user == "scissor" and comp == "paper"):
+    print(f"User {name}  wins the move {user}!")
+else: 
+    print(f"Machine wins with the move {comp}!")
