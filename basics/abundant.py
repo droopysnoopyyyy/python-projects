a03d58fd10992019276c09e71eb031d0fa8045be
Mondrita Ghosh <gmondrita.mg@gmail.com>
2025-08-16 17:16:03 +0530

    Delete basics/abundant.py

--- basics/abundant.py
+++
@@ -1,6 +0,0 @@
-def display(name,*subject):
-    print(name,"likes to read: ")
-    for i in subject:
-        print(i)
-display("Anik","Math","English")
-display("Priyanka","History","Bengali","Chemistry")
